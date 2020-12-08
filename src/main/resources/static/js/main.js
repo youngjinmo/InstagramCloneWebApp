05bc40c8cc8a18748304d178fc23348d661839c7
Youngjin Mo <dev.youngjinmo@gmail.com>
2020-12-08 17:33:30 +0900

    [data-name] 추가하고 like 버튼 활성화

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,40 @@
 const heart = document.querySelector('.heart_btn');
 const header = document.querySelector('#header');
 const sidebox = document.querySelector('.side_box');
+const delegation = document.querySelector('.contents_box');
+
 heart.addEventListener('click', function(){
     console.log('liked!');
     heart.classList.toggle('on');
+
 });
+
+function delegationFunc(e){
+    let elem = e.target;
+    console.log(e.target);
+
+    while(!elem.getAttribute('data-name')){
+        elem = elem.parentNode;
+        if(elem.nodeName === 'BODY'){
+            elem = null;
+            return;
+        }
+    }
+
+    if(elem.matches('[data-name="heartbeat"]')){
+        console.log('좋아요!');
+    }
+    else if(elem.matches('[data-name="bookmarked"]')){
+        console.log('북마크!');
+    }
+    else if(elem.matches('[data-name="share"]')){
+        console.log('공유!');
+    }
+    else if(elem.matches('[data-name="more"]')){
+        console.log('더보기');
+    }
+    elem.classList.toggle('on');
+}
 
 function resizeFunc(){
     if(pageYOffset>=10){
@@ -26,5 +56,6 @@ function scrollFunc(){
     }
 }
 
+window.addEventListener('click', delegationFunc);
 window.addEventListener('scroll', scrollFunc);
 window.addEventListener('scroll', scrollFunc);
