6d8d719d39fdd1b03f9c59fe59d9d886cce7f515
Youngjin Mo <dev.youngjinmo@gmail.com>
2020-12-08 14:34:53 +0900

    스크롤(pageYOffset)에 따라 header 고정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,17 @@
 const heart = document.querySelector('.heart_btn');
+const header = document.querySelector('#header');
+
 heart.addEventListener('click', function(){
     console.log('liked!');
     heart.classList.toggle('on');
 });
+
+function scrollFunc(){
+    console.log(pageYOffset);
+    if(pageYOffset>=10){
+        header.classList.add('on');
+    } else {
+        header.classList.remove('on');
+    }
+}
+window.addEventListener('scroll', scrollFunc);
