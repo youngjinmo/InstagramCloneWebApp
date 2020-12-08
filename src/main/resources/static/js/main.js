39d01962b7ae1d1fc9e14463bab5ff46adef3a4f
Youngjin Mo <dev.youngjinmo@gmail.com>
2020-12-08 16:41:38 +0900

    sidebox responsive design 적용

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,17 +1,31 @@
 const heart = document.querySelector('.heart_btn');
 const header = document.querySelector('#header');
+const sidebox = document.querySelector('.side_box');
 
 heart.addEventListener('click', function(){
     console.log('liked!');
     heart.classList.toggle('on');
 });
 
-function scrollFunc(){
-    console.log(pageYOffset);
+function resizeFunc(){
     if(pageYOffset>=10){
-        header.classList.add('on');
-    } else {
-        header.classList.remove('on');
+        const calcWith = (window.innerWidth*0.5)+180;
+        sidebox.style.left = calcWith+'px';
     }
 }
+
+function scrollFunc(){
+    
+    if(pageYOffset>=10){
+        header.classList.add('on');
+        sidebox.classList.add('on');
+        resizeFunc();
+    } else {
+        header.classList.remove('on');
+        sidebox.classList.remove('on');
+        sidebox.removeAttribute('style');
+    }
+}
+
+window.addEventListener('scroll', scrollFunc);
 window.addEventListener('scroll', scrollFunc);
